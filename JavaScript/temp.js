17ff1a89c9c2e3ed990bf2789a6fbb7049ef3bc4
Mitsuru Chiba <le.chibah@gmail.com>
2020-05-15 20:10:15 +0900

    titleCase kata with comments

---
+++ JavaScript/temp.js
@@ -0,0 +1,2 @@
+const testarr = "                  ".split(' ');
+console.log(testarr);
---
+++ JavaScript/titleCase.js
@@ -0,0 +1,33 @@
+function titleCase(title, minorWords) {
+    //creates a lowercase array with words in string
+    //initialize an array for the minorWords
+    let words = title.toLowerCase().split(' ');
+    let filter = [];
+    let newTitle = "";
+
+    //checks if there's a second argument (filter)
+    //then creates an array with lowercase filter words
+    if(minorWords) {
+        filter = minorWords.toLowerCase().split(' ');
+    };
+
+    //loops for each word of array Words
+    for(let i = 0; i < words.length; i++) {
+        const word = words[i];
+        //checks if it's the first word and
+        //if it's a filtered word
+        if(i != 0 && filter.includes(word)) {
+            newTitle += word + " ";
+        }
+
+        //checks if it's a valid word, then, title case it
+        else if(words[0]) {
+            newTitle += word[0].toUpperCase() + word.slice(1) + " ";
+        }
+    }
+
+    //removes extra blankspaces before returning the string
+    return newTitle.trim();
+}
+
+
