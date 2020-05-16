19ffc73a989713ead21379389b97c4f01f14455d
Mitsuru Chiba <le.chibah@gmail.com>
2020-05-17 02:32:05 +0900

    learned memoization for sumOfPairs

---
+++ JavaScript/sumOfPairs.js
@@ -0,0 +1,12 @@
+const sum_pairs = (ints, s) => {
+    let cache = {};
+    for(let m = 0; m < ints.length; m++) {
+        const find = s - ints[m];
+        if(cache[find] != undefined) {
+            return [find, cache[find]];
+        }
+        else {
+            cache[ints[m]] = find;
+        }
+    }
+}
